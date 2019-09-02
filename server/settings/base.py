ee1f5b6c8e2292936440c4ae70f9b7eae19a6cfa
Josmer Hernandez <jhernandez.18p@gmail.com>
2019-09-02 16:47:21 -0500

    Email conf

--- server/settings/base.py
+++ server/settings/base.py
@@ -132,9 +132,9 @@ else:
     """
     Email conf
     """
-    EMAIL_HOST = ''
-    EMAIL_PORT = ''
-    EMAIL_HOST_USER = ''
-    EMAIL_HOST_PASSWORD = ''
-    EMAIL_USE_TLS = True
-    DEFAULT_FROM_EMAIL = ''
+    EMAIL_HOST = config('EMAIL_HOST')
+    EMAIL_PORT = config('EMAIL_PORT')
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
