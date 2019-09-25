3d848706f7535e1c024bc113470de6ec70184995
Josmer Hernandez <jhernandez.18p@gmail.com>
2019-09-25 14:12:35 -0500

    seo

--- server/custom_context_processors/seo.py
+++ server/custom_context_processors/seo.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 def seo(request):
 
-    if settings.DEBUG:
+    if not settings.DEBUG:
         google_analytics_id = config('GOOGLE_ANALYTICS_ID')
         facebook_analytics_init = config('FACEBOOK_ANALYTICS_INIT')
         facebook_analytics_track = config('FACEBOOK_ANALYTICS_TRACK')
