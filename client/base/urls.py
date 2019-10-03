642f69c3284281b72c082bf4fc3ea49233aa0b12
Josmer Hernandez <jhernandez.18p@gmail.com>
2019-10-03 12:29:07 -0500

    updates

--- client/base/urls.py
+++ client/base/urls.py
@@ -23,4 +23,10 @@ urlpatterns = [
     path('suscribe', SuscribeView.as_view(), name='suscribe'),
     path('gracias', ThanksView.as_view(), name='thanks'),
     path('whatsapp', RedirectView.as_view(url='https://wa.me/50762667545'), name='whatsapp'),
+    path('go-to-instagram', RedirectView.as_view(url='https://www.instagram.com/dev2tech/'), name='social_instagram'),
+    path('go-to-facebook', RedirectView.as_view(url='https://www.facebook.com/dev2tech/'), name='social_facebook'),
+    path('go-to-twitter', RedirectView.as_view(url='https://www.twitter.com/dev2tech/'), name='social_twitter'),
+    path('instagram', RedirectView.as_view(url='/'), name='instagram'),
+    path('facebook', RedirectView.as_view(url='/'), name='facebook'),
+    path('twitter', RedirectView.as_view(url='/'), name='twitter'),
 ]
